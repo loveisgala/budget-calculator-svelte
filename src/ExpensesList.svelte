e00a0d59057aee6b5ddf3be2277aac261defc1e2
loveisgala <gabriela.gk@hotmail.com>
2021-04-18 16:53:47 -0300

    add animation

--- src/ExpensesList.svelte
+++ src/ExpensesList.svelte
@@ -1,7 +1,8 @@
 <script>
   import SectionTitle from "./components/Title.svelte";
   import Expense from "./Expense.svelte";
-  import { fly } from 'svelte/transition';
+  import { fly } from "svelte/transition";
+  import { flip } from "svelte/animate";
   export let expenses = [];
 </script>
 
@@ -9,9 +10,13 @@
   <SectionTitle title="expense list" />
   <ul>
     {#each expenses as expense, index (expense.id)}
-    <div in:fly={{ x: 200, delay: (index + 1) * 700}} out:fly={{ x: -200 }}>
-      <Expense {...expense}  />
-    </div>
+      <div
+        in:fly={{ x: 200, delay: (index + 1) * 700 }}
+        out:fly={{ x: -200 }}
+        animate:flip
+      >
+        <Expense {...expense} />
+      </div>
     {:else}
       <h2>currently you have no expenses</h2>
     {/each}
